d1171d33a3f111d2449c980f17fea60485dec524
zaitov <ramil@puzzle.kz>
2018-06-06 18:05:42 +0600

    Google Analytics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -152,6 +152,16 @@
         <link rel="stylesheet" href="/css/main.min.css">
     </noscript>
 
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120458217-1"></script>;
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-120458217-1');
+	</script>
+	
 </head>
 <body class="index-1" style="overflow: hidden;">
 	<div class="preloader">
